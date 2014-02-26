33b189faf22e8c6cd0094f0ab3b9ae9d4867c987
Yi <yi2004@gmail.com>
2014-02-26 16:37:47 +0800

    add pretty date

---
+++ public/js/jquery.prettydate-cn.js
@@ -0,0 +1,12 @@
+/* Chinese */
+$.extend($.prettyDate.messages, {
+	now: '刚刚',
+	minute: '1分钟前',
+	minutes: $.prettyDate.template('{0}分钟前'),
+	hour: '1小时前',
+	hours: $.prettyDate.template('{0}小时前'),
+	yesterday: '昨天',
+	days: $.prettyDate.template('{0}天前'),
+  week: '1星期前',
+	weeks: $.prettyDate.template('{0}星期前')
+});
